0653af22d40b6a4a21bbed229fb347aac3585698
Conor Owens <conorj.owens@gmail.com>
2019-07-28 17:53:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Welcome, TO ME!"
+  end
+  
+  get '/name' do
+    "My name is Conor Owens"
+  end
+  
+  get '/hometown' do
+    "My hometown is Chicago, Illinois."
+  end
+  
+  get '/favorite-song' do 
+    "My favorite song is Brandy by "
 end
